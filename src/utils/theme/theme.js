258416788dd424b93465e895bc65bd96828532d1
eunnnn <haeun0908@dsm.hs.kr>
2023-12-04 23:30:13 +0900

    ⚙️ :: Theme Setting

---
+++ src/utils/theme/theme.js
@@ -0,0 +1,128 @@
+import styled, { DefaultTheme } from "styled-components";
+
+export const colors = {
+  White: "#FFFFFF",
+  Ivory: "#FFFDF9",
+  Black: "#111111",
+  Pink: "#F86184",
+  Navy: "#1F4879",
+  Gray: {
+    900: "#202020",
+    800: "#414141",
+    700: "#606060",
+    600: "#747474",
+    500: "#9d9d9d",
+    400: "#bcbcbc",
+    300: "#dfdfdf",
+  },
+  Star: {
+    300: "#E9F6F8",
+    500: "#8DD0E4",
+    800: "#045FA3",
+  },
+  Cheese: {
+    300: "#FFFCF8",
+    500: "#EEBD85",
+    800: "#934635",
+  },
+  ChcoTree: {
+    300: "#BABF65",
+    500: "#884B31",
+    800: "#38251F",
+  },
+  Mint: {
+    300: "#D3EBEB",
+    500: "#9CD0CC",
+    800: "#3F2B14",
+  },
+  Almond: {
+    300: "#FFF9C7",
+    500: "#7D4B3C",
+    800: "#2B100C",
+  },
+  Alien: {
+    300: "#FCFAF4",
+    500: "#D5AD92",
+    800: "#390F05",
+  },
+  CottonCandy: {
+    300: "#FEFFD0",
+    500: "#FFDDE3",
+    800: "#D9EEF9",
+  },
+  LoveStrawberry: {
+    300: "#FFF2D0",
+    500: "#E92A41",
+    800: "#502200",
+  },
+  Raindow: {
+    300: "#FFFEFA",
+    500: "#FFC461",
+    800: "#E27C9D",
+  },
+  Wind: {
+    300: "#FFFDF9",
+    500: "#D63541",
+    800: "#941A34",
+  },
+  GreenTea: {
+    300: "#B8C577",
+    800: "#324822",
+  },
+  Oreo: {
+    300: "#EAE0D3",
+    800: "#50403E",
+  },
+  Vanilla: {
+    300: "#FFF5E0",
+    500: "#FFF4CE",
+    800: "#FFDB58",
+  },
+  Chocolate: {
+    300: "#674B38",
+    800: "#2D1A1B",
+  },
+  Cherry: {
+    300: "#FFE1E8",
+    800: "#D3545C",
+  },
+  BerryBerry: {
+    300: "#FFE9E0",
+    800: "#C30E22",
+  },
+};
+
+export const Bold32 = styled.p`
+  font-size: 32px;
+  font-weight: 700;
+`;
+export const Bold20 = styled.p`
+  font-size: 20px;
+  font-weight: 700;
+`;
+export const SemiBold24 = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+`;
+export const SemiBold22 = styled.p`
+  font-size: 22px;
+  font-weight: 600;
+`;
+export const SemiBold20 = styled.p`
+  font-size: 20px;
+  font-weight: 600;
+`;
+export const Medium20 = styled.p`
+  font-size: 20px;
+  font-weight: 500;
+`;
+export const Medium16 = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+`;
+export const Regular20 = styled.p`
+  font-size: 20px;
+  font-weight: 400;
+`;
+
+export default theme;
